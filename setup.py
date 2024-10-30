951968ace9df0787ae38434ddd4cd0011de711bd
ocbyram <ocb3wv@virginia.edu>
2024-10-29 20:23:13 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/ocbyram/ds5100-module9challenge',
+    author='Olivia Byram',
+    author_email='ocbyram@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
